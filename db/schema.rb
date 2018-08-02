177ccda2d1a14ab9bc946869157ec53eb9d1e6a6
bwadlow <bwadlow@gmail.com>
2018-08-02 15:36:46 -0400

    latest change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180731165557) do
+ActiveRecord::Schema.define(version: 2018_07_31_165557) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180731165557) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "price"
   end
 
